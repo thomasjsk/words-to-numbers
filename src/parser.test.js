35cb1d746b9e0991b1f98a126da7cabfa5349d69
Thomasjsk <thomas.jasiuk@gmail.com>
2021-10-26 22:06:30 +0200

    Multiplies single worded numbers.

--- src/parser.test.js
+++ src/parser.test.js
@@ -30,22 +30,31 @@ describe("match word", () => {
   });
 
   describe("multipliers", () => {
-    it("should multiply by 100 if matched hundred", () => {
-      const numberString = "five hundred";
-
-      expect(parse(numberString)).toEqual(500);
-    });
-
-    it("should multiply by 1000 if matched thousand", () => {
-      const numberString = "seven thousand";
-
-      expect(parse(numberString)).toEqual(7000);
-    });
-
-    it("should multiply by 1000000 if matched million", () => {
-      const numberString = "two million";
-
-      expect(parse(numberString)).toEqual(2000000);
+    describe("single word number to multiply", () => {
+      [
+        {
+          string: "five hundred",
+          multiplierString: "hundred",
+          multiplier: 100,
+          expected: 500,
+        },
+        {
+          string: "seventeen thousand",
+          multiplierString: "thousand",
+          multiplier: 1000,
+          expected: 17000,
+        },
+        {
+          string: "eleven million",
+          multiplierString: "million",
+          multiplier: 1000000,
+          expected: 11000000,
+        },
+      ].forEach(({ string, multiplierString, multiplier, expected }) => {
+        it(`should multiply by ${multiplier} if matched ${multiplierString}`, () => {
+          expect(parse(string)).toEqual(expected);
+        });
+      });
     });
   });
 });
